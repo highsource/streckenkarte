26a944b3d9790ff9b4a15da4764697b4ce80b814
Alexey Valikov <aleksei.valikov@gmail.com>
2018-07-07 17:32:51 +0200

    Updated the  IC/EC map.

---
+++ IC_EC-Strecken_2018/download-data.bat
@@ -0,0 +1,2 @@
+md data
+wget -P data --no-check-certificate https://www.bahn.de/p/view/mdb/bahnintern/fahrplan_und_buchung/reiseauskunftsmedien/fahrplanmedien-download/mdb_263335_icec_liniennetz_2018.pdf
