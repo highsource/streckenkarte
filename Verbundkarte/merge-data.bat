8fa124138ba07e3afe0b9bb3a7903c82e3ddb6a3
Alexey Valikov <aleksei.valikov@gmail.com>
2018-07-07 18:11:51 +0200

    Added the Verbundkarte.

---
+++ Verbundkarte/merge-data.bat
@@ -0,0 +1,20 @@
+set IMAGEMAGICK_PATH="C:\Tools\ImageMagick-1"
+md images
+%IMAGEMAGICK_PATH%\magick images\t-0-0.jpg images\t-1-0.jpg images\t-2-0.jpg images\t-3-0.jpg images\t-4-0.jpg images\t-5-0.jpg +append images\t-0.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-1.jpg images\t-1-1.jpg images\t-2-1.jpg images\t-3-1.jpg images\t-4-1.jpg images\t-5-1.jpg +append images\t-1.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-2.jpg images\t-1-2.jpg images\t-2-2.jpg images\t-3-2.jpg images\t-4-2.jpg images\t-5-2.jpg +append images\t-2.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-3.jpg images\t-1-3.jpg images\t-2-3.jpg images\t-3-3.jpg images\t-4-3.jpg images\t-5-3.jpg +append images\t-3.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-4.jpg images\t-1-4.jpg images\t-2-4.jpg images\t-3-4.jpg images\t-4-4.jpg images\t-5-4.jpg +append images\t-4.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-5.jpg images\t-1-5.jpg images\t-2-5.jpg images\t-3-5.jpg images\t-4-5.jpg images\t-5-5.jpg +append images\t-5.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-6.jpg images\t-1-6.jpg images\t-2-6.jpg images\t-3-6.jpg images\t-4-6.jpg images\t-5-6.jpg +append images\t-6.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-7.jpg images\t-1-7.jpg images\t-2-7.jpg images\t-3-7.jpg images\t-4-7.jpg images\t-5-7.jpg +append images\t-7.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-8.jpg images\t-1-8.jpg images\t-2-8.jpg images\t-3-8.jpg images\t-4-8.jpg images\t-5-8.jpg +append images\t-8.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-9.jpg images\t-1-9.jpg images\t-2-9.jpg images\t-3-9.jpg images\t-4-9.jpg images\t-5-9.jpg +append images\t-9.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0-10.jpg images\t-1-10.jpg images\t-2-10.jpg images\t-3-10.jpg images\t-4-10.jpg images\t-5-10.jpg +append images\t-10.jpg
+%IMAGEMAGICK_PATH%\magick images\t-0.jpg images\t-1.jpg images\t-2.jpg images\t-3.jpg images\t-4.jpg images\t-5.jpg images\t-6.jpg images\t-7.jpg images\t-8.jpg images\t-9.jpg images\t-10.jpg -append images\t.jpg
+%IMAGEMAGICK_PATH%\convert images\t.jpg -resize   x256 images\0.jpg
+%IMAGEMAGICK_PATH%\convert images\t.jpg -resize   x512 images\1.jpg
+%IMAGEMAGICK_PATH%\convert images\t.jpg -resize  x1024 images\2.jpg
+%IMAGEMAGICK_PATH%\convert images\t.jpg -resize  x2048 images\3.jpg
+%IMAGEMAGICK_PATH%\convert images\t.jpg -resize  x4096 images\4.jpg
+%IMAGEMAGICK_PATH%\convert images\t.jpg -resize  x8192 images\5.jpg
